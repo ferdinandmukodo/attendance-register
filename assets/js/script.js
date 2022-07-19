a6d6e9cf2f5b9a1f19ec7268b07c2f1356ff3cc0
bryant-kanda <brian.kandagor@student.moringaschool.com>
2022-07-19 14:21:37 +0300

    merge html with main

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const btn=document.getElementById("log_In")
+btn.addEventListener("click",(event)=>{
+    event.preventDefault()
+let inputA=document.getElementById("Admin_email").value
+let inputB=document.getElementById("Password").value})
