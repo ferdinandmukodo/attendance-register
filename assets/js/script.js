b30ac89589e352cf226ecf2b8cce91ffaa1d1f93
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-07-20 14:59:48 +0300

    It adds javascript code

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+
+function myFunctiona() {
+    var check = document.getElementsByName("")
+}
+
+function myFunction() {
+    let =
+}
