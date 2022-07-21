a8d115221f991406157a7422b7c96e50889d9f25
bryant-kanda <brian.kandagor@student.moringaschool.com>
2022-07-21 11:00:36 +0300

    introduce hash on password

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,3 +3,11 @@ btn.addEventListener("click",(event)=>{
     event.preventDefault()
 let inputA=document.getElementById("Admin_email").value
 let inputB=document.getElementById("Password").value})
+
+
+ function hashPassword(){
+    let salt=bcrypt.genSaltSync(10);
+    let hash=bcrypt.hashSync(password,salt)
+    return hash;
+
+ }
